770bed8973ace45b5ba4b02ec0e8d4da679064ed
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2026-03-10 11:01:29 +0700

    menampilkan proses sorting (

--- insertione.cpp
+++ insertione.cpp
@@ -40,5 +40,17 @@ void insertinosort()//procedure insertion sort
     {
         temp = arr[i];
         j = i - 1;
-    } 
-}   
+    while (j >= 0 && arr[j]> temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1]= temp;
+        cout << "\nStep" << i << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k]<< " ";
+        }
+    }
+}
+
